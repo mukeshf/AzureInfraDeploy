7967a6ee2590ade78212931e2bb06fc9dfc4ad6f
mukeshf <mukeshfulewale@gmail.com>
2018-02-12 10:20:05 +0530

    Delete CopyFilesfrom-AzuretoVM.ps1

--- CopyFilesfrom-AzuretoVM.ps1
+++
@@ -1,4 +0,0 @@
-﻿md -Force C:\Scripts | Out-Null
-$URL = "https://1imagestore.blob.core.windows.net/scripts/PasswordPolicy.ps1"
-$file = "C:\Scripts\PasswordPolicy.ps1"
-Invoke-WebRequest -Uri $URL -OutFile $file
